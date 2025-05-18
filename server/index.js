30f758b833d6cf6c1c540bb014131acc32398162
Marta Markiv <mpat@keplercode.com>
2025-05-18 09:27:34 +0300

    Updated gitignore file, server initialisation

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({message: "Hello World!"});
+});
+
+app.listen(port, () => {
+  console.log(`Chat app listening on port ${port}`);
+})
